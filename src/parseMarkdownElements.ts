d53d2f1daa0820abf4f4369c50d4f8881e7ca82c
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-29 10:27:15 +0100

    refactor

--- src/parseMarkdownElements.ts
+++ src/parseMarkdownElements.ts
@@ -5,6 +5,19 @@ const BREAKPOINT = "\n";
 
 const GET_ENTIRE_HEADING_LEVEL_REGEX = /#/g; // This will match all #s. `g` means global, so it will match all #s.
 
+type HeadingLevel = GetTypesStartingWithPrefix<MarkdownElement, "h">;
+
+function getHeadingLevel(textareaContent: string) {
+  return textareaContent.match(GET_ENTIRE_HEADING_LEVEL_REGEX)
+    ?.length as number;
+}
+
+function getHeadingEnd(textareaContent: string, headingStart: number) {
+  return textareaContent.includes(BREAKPOINT)
+    ? textareaContent.indexOf(BREAKPOINT, headingStart)
+    : textareaContent.length;
+}
+
 export function parseMarkdownElements(
   textareaContent: string
 ): MarkdownElement[] {
@@ -12,29 +25,20 @@ export function parseMarkdownElements(
 
   for (let i = 0; i < textareaContent.length; i++) {
     if (textareaContent[i] === "#") {
-      // 1. Get heading level via counting number of #s through Regex
-      const headingLevel = textareaContent.match(GET_ENTIRE_HEADING_LEVEL_REGEX)
-        ?.length as number;
+      // Get heading level via counting number of #s through Regex
+      const headingLevel = getHeadingLevel(textareaContent.slice(i));
 
-      // 2. If #s do not end with space, then we do not want to parse yet
+      // If #s do not end with space, then we do not want to parse yet
       if (textareaContent[i + headingLevel] !== " ") {
         continue;
       }
 
-      // 3. If #s do end with space, then we want to parse the heading
       const headingStart = i + headingLevel + 1;
-
-      const headingEnd = textareaContent.includes(BREAKPOINT)
-        ? textareaContent.indexOf(BREAKPOINT, headingStart)
-        : textareaContent.length;
-
+      const headingEnd = getHeadingEnd(textareaContent, headingStart);
       const headingContent = textareaContent.slice(headingStart, headingEnd);
 
       elements.push({
-        type: `h${headingLevel}` as GetTypesStartingWithPrefix<
-          MarkdownElement,
-          "h"
-        >,
+        type: `h${headingLevel}` as HeadingLevel,
         content: headingContent,
         id: v4(),
       });
