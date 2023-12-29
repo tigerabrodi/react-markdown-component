d1736c07e931089b8288dcf4b2205d5c7191fc31
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-29 08:18:41 +0100

    more styles and setup where content will be previewed

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,15 @@ export function App() {
       <h1>Markdown Preview Playground</h1>
       <div className="wrapper">
         <div className="markdown">
-          <h2>Markdown Textarea</h2>
+          <label htmlFor="markdown">Enter markdown here</label>
+          <textarea placeholder="# This is a title" id="markdown" />
         </div>
 
         <div className="preview">
           <h2>Preview</h2>
+          <div className="content">
+            this is where the preview will be rendered
+          </div>
         </div>
       </div>
     </main>
