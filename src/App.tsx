b45b968a120019d9a31ce4b024d52309ed7e6b9e
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-29 08:47:54 +0100

    setup markdown element

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,57 @@
+import { useCallback, useEffect, useState } from "react";
+import debounce from "lodash.debounce";
 import "./App.css";
 
+type MarkdownElement = {
+  // TODO: add more types
+  type: "h1";
+  content: string;
+  id: string;
+};
+
 export function App() {
+  const [textareaContent, setTextareaContent] = useState("");
+  const [markdownElements, setMarkdownElements] = useState<MarkdownElement[]>(
+    []
+  );
+
+  // useCallback is required for debounce to work
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedMarkdownOnTextareaChange = useCallback(
+    debounce(async (textareaContent: string) => {
+      // do something with the textareaContent
+      console.log(textareaContent);
+    }, 500),
+
+    []
+  );
+
+  useEffect(() => {
+    debouncedMarkdownOnTextareaChange(textareaContent);
+  }, [textareaContent, debouncedMarkdownOnTextareaChange]);
+
   return (
     <main>
       <h1>Markdown Preview Playground</h1>
       <div className="wrapper">
         <div className="markdown">
           <label htmlFor="markdown">Enter markdown here</label>
-          <textarea placeholder="# This is a title" id="markdown" />
+          <textarea
+            placeholder="# This is a title"
+            id="markdown"
+            value={textareaContent}
+            onChange={(event) => setTextareaContent(event.target.value)}
+          />
         </div>
 
         <div className="preview">
           <h2>Preview</h2>
           <div className="content">
-            this is where the preview will be rendered
+            {markdownElements.map((markdownElement, index) => (
+              <markdownElement.type key={markdownElement.id}>
+                {markdownElement.content}
+              </markdownElement.type>
+            ))}
           </div>
         </div>
       </div>
