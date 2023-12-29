3b465b39b1e4ee9b43d9b9644ba54d77e48e7f01
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-29 18:03:31 +0100

    types

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,15 @@ export type GetTypesStartingWithPrefix<
   Prefix extends string
 > = Type extends `${Prefix}${infer _}` ? Type : never;
 
+type Tag = {
+  type: "normal" | "bold";
+  content: string;
+};
+
 export type MarkdownElement =
   | {
       type: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
-      content: string;
+      content: Array<Tag>;
       id: string;
     }
   | {
