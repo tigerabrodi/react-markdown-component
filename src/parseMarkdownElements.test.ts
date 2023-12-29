b8f8f911609a37f85c3512699c083cbc300102c7
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-29 17:21:36 +0100

    more tests

--- src/parseMarkdownElements.test.ts
+++ src/parseMarkdownElements.test.ts
@@ -78,4 +78,116 @@ describe("paragraphs", () => {
       },
     ] satisfies MarkdownElement[]);
   });
+
+  it("should parse a single paragraph with a breakpoint", () => {
+    const markdown = "Hello World\n\n";
+    const elements = parseMarkdownElements(markdown);
+    expect(elements).toEqual([
+      {
+        type: "p",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+    ] satisfies MarkdownElement[]);
+  });
+
+  it("should parse multiple paragraphs with many breakpoints", () => {
+    const markdown = "Hello World\n\nHello World\n\nHello World\n\n";
+    const elements = parseMarkdownElements(markdown);
+    expect(elements).toEqual([
+      {
+        type: "p",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+      {
+        type: "p",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+      {
+        type: "p",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+    ] satisfies MarkdownElement[]);
+  });
+
+  it("should parse multiple paragraphs with many breakpoints and headings", () => {
+    const markdown =
+      "# Hello World\n\nHello World\n\n## Hello World\n\nHello World\n\n### Hello World\n\nHello World\n\n";
+    const elements = parseMarkdownElements(markdown);
+    expect(elements).toEqual([
+      {
+        type: "h1",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+      {
+        type: "p",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+      {
+        type: "h2",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+      {
+        type: "p",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+      {
+        type: "h3",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+      {
+        type: "p",
+        content: "Hello World",
+        id: expect.any(String),
+      },
+      {
+        type: "breakpoint",
+        id: expect.any(String),
+      },
+    ] satisfies MarkdownElement[]);
+  });
 });
