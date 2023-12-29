f31a3376de50e09c0725ceb44dc80ab09ddf23c0
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-29 07:47:15 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Markdown Live Preview Component
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+export function App() {
+  return (
+    <main>
+      <h1>Hello, world!</h1>
+      <div className="wrapper">
+        <div className="markdown">
+          <h2>Markdown Textarea</h2>
+        </div>
+
+        <div className="preview">
+          <h2>Preview</h2>
+        </div>
+      </div>
+    </main>
+  );
+}
